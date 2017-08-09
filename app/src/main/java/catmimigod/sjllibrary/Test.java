53b181d3637b88883206b99ef75083b02a7aff5d
CatmimiGod <catmimishen@qq.com>
2017-08-09 14:16:48 +0800

    Initial commit

---
+++ app/src/main/java/catmimigod/sjllibrary/Test.java
@@ -0,0 +1,12 @@
+package catmimigod.sjllibrary;
+
+/**
+ * Created by catmi on 2017/8/9.
+ */
+
+public class Test {
+
+    public static String getText(){
+        return "This is SJLLibrary";
+    }
+}
